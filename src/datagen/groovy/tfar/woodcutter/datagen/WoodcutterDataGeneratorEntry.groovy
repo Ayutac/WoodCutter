5951b3af50cdedcf11030a955a2e83f1a1e5746a
ayutac <fly.high.android@gmail.com>
2022-12-09 20:26:57 +0100

    added datagen base

---
+++ src/datagen/groovy/tfar/woodcutter/datagen/WoodcutterDataGeneratorEntry.groovy
@@ -0,0 +1,13 @@
+package tfar.woodcutter.datagen
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+import org.slf4j.LoggerFactory
+
+class WoodcutterDataGeneratorEntry implements DataGeneratorEntrypoint {
+
+    @Override
+    void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
